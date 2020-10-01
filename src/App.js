effb36840ee980d5ee71273518af589039dd9dd0
Anushka <anusingh1883656@gmail.com>
2020-10-02 04:36:26 +0530

    Routing App

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Shop from "./shop";
+import About from "./about";
+import Nav from "./nav";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Nav />
+        <Switch>
+          {/* //Switch stop the unnecassary routing in pages */}
+          <Route path="/" exact component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/shop" component={Shop} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
-
+const Home = () => (
+  <div>
+    <h1>Homepage</h1>
+  </div>
+);
 export default App;
